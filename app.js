e65beadea8824b98ed3b6f83d1942333d207bd26
mikeovas <mikeovas@gmail.com>
2021-08-24 13:45:58 -0400

    phrases added

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ const btn_reset = document.querySelector('.btn_reset');
 
 // in-game variables
 let missed_guess = 0;
+
+//arrays of phrases to select letters from
+const phrases = [
+    'not all who wander are lost',
+    'you shall not pass',
+    'with great power comes great responsibility',
+    'there is no place like home',
+    'i can do this all day',
+    'live long and prosper',
+    'peace and long life',
+    'the only failure is not to try',
+    'to infinity and beyond',
+    'do not be afraid to fail',
+];
