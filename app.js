766efe53c092d0b9e06ccedc9c0e5513995143a6
mikeovas <mikeovas@gmail.com>
2021-08-24 12:20:57 -0400

    select variables

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("Testing link");
+//select variables
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const btn_reset = document.querySelector('.btn_reset');
+
+
+// in-game variables
+let missed_guess = 0;
