47939edb4c15bef3ec21507131d39c13e58e891d
mikeovas <mikeovas@gmail.com>
2021-09-26 14:45:29 -0400

    updated comments

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ const qwerty = document.getElementById('qwerty');
 const phraseDiv = document.getElementById('phrase');
 const phraseUl = phraseDiv.children[0];
 const reset_game = document.querySelector('.btn__reset');
-
 const heartTemplate = '<li class="tries"><img src="images/liveHeart.png" height="35px" width="30px"></li>';
-
 
 
 // variable to keep track of missed guesses
 let missed_guesses = 0;
 const max_guesses = 5;
 updateHearts();
-
-
 
 
 //arrays of phrases to select letters from
@@ -38,6 +34,8 @@ reset_game.addEventListener('click', () => {
     overlay.style.display = "none";
 });
 
+
+//Functions
 
 //function to select a random phrase for the game and split phrase into letters
 function getRandomPhraseAsArray(arr) {
@@ -82,7 +80,6 @@ function checkLetter(choice) {
             match = true;
         }
     }
-
     console.log(match);
     return match;
 }
@@ -91,15 +88,14 @@ function updateHearts() {
 
     const lives = document.getElementById('lives');
     lives.innerHTML = '';
-
     numHearts = max_guesses - missed_guesses; //allows to change number of missed guesses as desired
-
     for (i = 0; i < numHearts; i++) {
         lives.insertAdjacentHTML('beforeend', heartTemplate);
     }
-
 }
 
+
+//Event Listeners
 
 // the event listener for the qwerty element to select a letter to guess and calls the checkLetter function to check if the choice is correct
 qwerty.querySelectorAll('button').forEach((element) => {
